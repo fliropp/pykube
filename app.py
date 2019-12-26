0c03858561eb04b216ac63c68e52cdd8336509ef
fliropp <grottum@gmail.com>
2019-12-26 13:54:54 +0100

    git pomfrittes

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
+from flask import jsonify
 from redis import Redis
+from datetime import datetime
+import json
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis-master', port=6379)
+#redis = Redis(host='localhost', port=6379)
+
+
+def get_response():
+    dateTimeObj = datetime.now()
+    tstr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+    jdict = {
+    'hits': redis.get('hits'),
+    'source': 'Redis',
+    'timestamp': tstr
+    }
+    jdict = json.dumps(jdict)
+    resp = json.loads(jdict)
+    return resp
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
-    return 'greetings from kube-py'
+    r = get_response()
+    return jsonify(r), 200
 
 
 if __name__ == "__main__":
